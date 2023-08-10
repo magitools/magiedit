ec016a13fa4390de74bdf3738dda06698c2f6ed4
matfire <mgassend@gmail.com>
2023-08-10 08:04:40 +0000

    add: hashnode check

--- src/lib/articles/publish.ts
+++ src/lib/articles/publish.ts
@@ -1,13 +1,17 @@
 import { db } from "../storage/db"
 import { DevPlatform } from "./platforms/dev";
+import { HashnodePlatform } from "./platforms/hashnode";
 
 export const publish = async (id: string) => {
     const article = await db.articles.get(id);
     if (!article) return;
     const settings = await db.settings.toArray();
 
-    if (settings.find((e) => e.name === "dev_token")) {
+/*     if (settings.find((e) => e.name === "dev_token")) {
         await new DevPlatform().publish(article)
+    } */
+    if (settings.find((e) => e.name === "hashnode_token")) {
+        await new HashnodePlatform().publish(article)
     }
 
 }
