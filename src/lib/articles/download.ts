f0a5bce953fbe47fd8add661957ef09f5259d37d
matfire <mgassend@gmail.com>
2023-08-17 13:28:04 +0000

    fix: no longer adding # title on download

--- src/lib/articles/download.ts
+++ src/lib/articles/download.ts
@@ -3,7 +3,7 @@ import { db } from "$lib/storage/db";
 export const generateArticleBlob = async (id: number) : Promise<Blob> => {
     const article = await db.articles.get(id);
 
-    const text = `# ${article?.title}\n\n${article?.content}`
+    const text = `${article?.content}`
 
     const blob = new Blob([text], {type: "text/plain"});
 
