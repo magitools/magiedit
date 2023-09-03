07d5f7a6f3ad40f6349343954af1c42f4cecb842
Matteo Gassend <mgassend@gmail.com>
2023-09-03 10:04:10 +0200

    fix: pnpm on gh action

--- ci/build.mjs
+++ ci/build.mjs
@@ -1,10 +1,19 @@
-import {connect} from "@dagger.io/dagger"
+import { connect } from '@dagger.io/dagger';
 
-connect(async (client) => {
-    const node = await client.container().from("node:18-alpine").withDirectory("/app", client.host().directory("."), {
-        exclude: ["node_modules"]
-    })
+connect(
+	async (client) => {
+		const node = await client
+			.container()
+			.from('node:18-alpine')
+			.withDirectory('/app', client.host().directory('.'), {
+				exclude: ['node_modules']
+			});
 
-    const runner = node.withWorkdir("/app").withExec(["npm", "install"])
-    const out = await runner.withEnvVariable("NODE_BUILD", "y").withExec(["npm", "run", "build"]).stderr()
-}, {LogOutput: process.stdout})
+		const runner = node.withWorkdir('/app').withExec(['npm', 'install']);
+		const out = await runner
+			.withEnvVariable('NODE_BUILD', 'y')
+			.withExec(['npm', 'run', 'build'])
+			.stderr();
+	},
+	{ LogOutput: process.stdout }
+);
--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,6 @@ import adapter from '@sveltejs/adapter-auto';
 import nodeAdapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-
 const buildOnNode = process.env.NODE_BUILD !== null;
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -18,7 +17,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: buildOnNode ? nodeAdapter() : adapter(),
+		adapter: buildOnNode ? nodeAdapter() : adapter()
 	}
 };
 export default config;
