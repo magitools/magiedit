64af065b06e2d8ebd89661cafc68fddf878a20af
Matteo Gassend <matteo.gassend@agiltech.fr>
2023-09-05 15:10:18 +0200

    add: load file, save to db and redirect to edit page

--- src/routes/write/load/+page.svelte
+++ src/routes/write/load/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { db } from '$lib/storage/db';
+	import { getToastStore } from '@skeletonlabs/skeleton';
 	import { showOpenFilePicker } from 'file-system-access';
-
+	const toastStore = getToastStore();
 	async function handleFileLoad() {
 		const [filehandle] = await showOpenFilePicker({
 			accepts: [
@@ -18,6 +21,24 @@
 			]
 		});
 		if (!filehandle) return;
+		try {
+			const content = await (await filehandle.getFile()).text();
+			const id = await db.articles.add({
+				title: filehandle.name,
+				content,
+				tags: []
+			});
+			toastStore.trigger({
+				message: 'file loaded'
+			});
+			await goto(`/write/${id}`);
+		} catch (error) {
+			toastStore.trigger({
+				message: 'could not open file, please try again'
+			});
+			console.error(error);
+			return;
+		}
 	}
 </script>
 
