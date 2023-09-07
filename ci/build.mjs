b63de97e08665419b1ca0ecdac8063ccbcd856f4
Matteo Gassend <matteo.gassend@agiltech.fr>
2023-09-07 14:17:27 +0200

    fix: build script env vars

--- ci/build.mjs
+++ ci/build.mjs
@@ -5,18 +5,18 @@ connect(
 		const nodeCache = client.cacheVolume('node');
 		const node = await client
 			.container()
-			.from('node:18-alpine')
+			.from('node:18')
 			.withDirectory('/app', client.host().directory('.'), {
 				exclude: ['node_modules', 'ci']
 			})
 			.withMountedCache('/app/node_modules', nodeCache);
 
-		const runner = node.withWorkdir('/app').withExec(['npm', 'install']);
+		const runner = node.withWorkdir('/app').withExec(['npm', 'install', '--ignore-scripts']);
 		await runner
 			.withEnvVariable('BUILD_ON_NODE', 'y')
 			.withEnvVariable('GIPHY_TOKEN', 'giphy_token')
 			.withEnvVariable('UNSPLASH_TOKEN', 'unsplash_token')
-			.withEnvVariable('DATABASE_URL', 'url')
+			.withEnvVariable('DATABASE_URL', 'libsql://fake-url.com')
 			.withEnvVariable('DATABASE_TOKEN', 'token')
 			.withEnvVariable('GITHUB_CLIENT_ID', 'id')
 			.withEnvVariable('GITHUB_CLIENT_SECRET', 'secret')
