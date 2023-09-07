9da5fb415b62ffb53c77e6c932666fae9a6f9dc5
Matteo Gassend <matteo.gassend@agiltech.fr>
2023-09-07 14:32:28 +0200

    add: basic styling for login page

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -3,11 +3,22 @@
 </script>
 
 <div class="w-full h-full flex flex-col justify-center items-center">
-	<div class="card p-4">
-		<a class="btn variant-filled" href="/login/github">
-			<span><GithubIcon /></span>
-			<span>Sign in with Github</span>
-		</a>
+	<div class="card flex flex-col">
+		<div class="card-header">
+			<p class="text-xl font-bold">Join MagiEdit</p>
+		</div>
+		<div class="p-4">
+			<p>
+				Creating and logging into an account is used to synchronize your notes and make them
+				available on other devices.
+			</p>
+			<div class="flex justify-evenly mt-4">
+				<a class="btn variant-filled" href="/login/github">
+					<span><GithubIcon /></span>
+					<span>Sign in with Github</span>
+				</a>
+			</div>
+		</div>
 	</div>
 </div>
 
