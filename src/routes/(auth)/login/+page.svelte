bede0f6f1d363faa7b2f1e49af942f4f7abf6d29
Matteo Gassend <matteo.gassend@agiltech.fr>
2023-09-07 11:53:56 +0200

    add: styling for login page wip

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+	import GithubIcon from '~icons/mdi/github';
+</script>
+
+<div class="w-full h-full flex flex-col justify-center items-center">
+	<div class="card p-4">
+		<a class="btn variant-filled" href="/login/github">
+			<span><GithubIcon /></span>
+			<span>Sign in with Github</span>
+		</a>
+	</div>
+</div>
+
 <h1>Sign in</h1>
-<a href="/login/github">Sign in with Github</a>
