e9c3bb763ef96ba4f80f5a79e78af817cf94d053
Matteo Gassend <mgassend@gmail.com>
2023-09-16 12:29:24 +0200

    fix: back to generic any for base platform

--- src/lib/articles/platforms/base.ts
+++ src/lib/articles/platforms/base.ts
@@ -1,8 +1,8 @@
 import type { UserPreferences } from '$lib/server/drizzle';
 import type { Article } from '$lib/storage/db';
 
-export const supportedPlatforms: (new () => IBasePlatform<unknown>)[] = [];
-export function RegisterPlatform(constructor: new () => IBasePlatform<unknown>) {
+export const supportedPlatforms: (new () => IBasePlatform<any>)[] = [];
+export function RegisterPlatform(constructor: new () => IBasePlatform<any>) {
 	supportedPlatforms.push(constructor);
 }
 
