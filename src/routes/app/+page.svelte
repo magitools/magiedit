5d4563ee0012b4aecc81e52d4e5e44d16d42a239
Matteo Gassend <matteo.gassend@agiltech.fr>
2023-09-21 09:48:45 +0200

    add: page layout wip

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -4,14 +4,14 @@
 	export let data;
 </script>
 
+<div class="flex justify-center items-center space-x-4">
+	<a class="btn variant-filled" href="/app/write/new">Create</a>
+	<span>or</span>
+	<a class="btn variant-filled" href="/app/write/load">Load</a>
+</div>
 {#if data.articles.length === 0}
 	<div class="flex flex-col items-center h-full justify-center">
 		<p>looks like you don't have any articles</p>
-		<div>
-			<a class="btn variant-filled" href="/app/write/new">Create</a>
-			or
-			<a class="btn variant-filled" href="/app/write/load">Load</a>
-		</div>
 	</div>
 {:else}
 	<div class="flex flex-wrap">
