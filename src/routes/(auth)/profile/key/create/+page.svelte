aef1f1341afc984071c8858385cdda451fc7b3d0
Matteo Gassend <mgassend@gmail.com>
2023-10-10 16:17:16 +0200

    add: loading state + error toast

--- src/routes/(auth)/profile/key/create/+page.svelte
+++ src/routes/(auth)/profile/key/create/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import LoadingOverlay from '$lib/components/LoadingOverlay.svelte';
+	import { getToastStore } from '@skeletonlabs/skeleton';
 
 	let passkey = '';
+	let loading = false;
+	const toastStore = getToastStore();
+
 	async function handleSubmit(ev: SubmitEvent) {
 		ev.preventDefault();
+		loading = true;
 		const data = new FormData();
 		data.append('passkey', passkey);
 		const res = await fetch('/api/key/create', {
@@ -12,6 +18,8 @@
 		});
 		if (!res.ok) {
 			console.log('error');
+			toastStore.trigger({ message: 'something went wrong, pleasy try again later' });
+			loading = false;
 			return;
 		}
 		sessionStorage.setItem('magiedit:key', passkey);
@@ -19,22 +27,31 @@
 	}
 </script>
 
-<p>
-	Almost done! you just need to choose a master encryption key; this will be used to securely store
-	articles on our servers. Be careful to not loose it
-</p>
-
-<form action="" method="post" on:submit={handleSubmit}>
-	<label for="passkey">
-		<span>Passkey</span>
-		<input
-			required
-			bind:value={passkey}
-			type="password"
-			name="passkey"
-			autocomplete="new-password"
-			class="input"
-		/>
-	</label>
-	<button type="submit" class="btn variant-filled">Submit</button>
-</form>
+<div class="flex justify-center items-center h-full">
+	<div class="card min-h-[200px] relative">
+		{#if loading}
+			<LoadingOverlay text="checking and unlocking content, please wait" />
+		{/if}
+		<div class="card-header text-xl font-bold">Almost there!</div>
+		<div class="p-4 space-y-2">
+			<p>
+				you just need to choose a master encryption key; this will be used to securely store
+				articles on our servers. Be careful to not loose it
+			</p>
+			<form method="post" on:submit={handleSubmit}>
+				<label for="passkey">
+					<span>Passkey</span>
+					<input
+						disabled={loading}
+						bind:value={passkey}
+						type="password"
+						name="passkey"
+						autocomplete="new-password"
+						class="input"
+					/>
+				</label>
+				<button disabled={loading} type="submit" class="btn variant-filled mt-2">Submit</button>
+			</form>
+		</div>
+	</div>
+</div>
--- src/routes/(auth)/profile/key/unlock/+page.svelte
+++ src/routes/(auth)/profile/key/unlock/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import LoadingOverlay from '$lib/components/LoadingOverlay.svelte';
+	import { getToastStore } from '@skeletonlabs/skeleton';
 
 	let passkey = '';
+	let loading = false;
+	const toastStore = getToastStore();
 	async function handleSubmit(ev: SubmitEvent) {
 		ev.preventDefault();
+		loading = true;
 		const data = new FormData();
 		data.append('passkey', passkey);
 		const res = await fetch('/api/key/unlock', {
@@ -11,7 +16,9 @@
 			body: data
 		});
 		if (!res.ok) {
-			console.log('error');
+			console.log(await res.json());
+			loading = false;
+			toastStore.trigger({ message: 'something went wrong, pleasy try again later' });
 			return;
 		}
 		sessionStorage.setItem('magiedit:key', passkey);
@@ -19,18 +26,28 @@
 	}
 </script>
 
-<p>Almost done! please enter the key you previously created to unlock your articles</p>
-
-<form action="" method="post" on:submit={handleSubmit}>
-	<label for="passkey">
-		<span>Passkey</span>
-		<input
-			bind:value={passkey}
-			type="password"
-			name="passkey"
-			autocomplete="current-password"
-			class="input"
-		/>
-	</label>
-	<button type="submit" class="btn variant-filled">Submit</button>
-</form>
+<div class="flex justify-center items-center h-full">
+	<div class="card min-h-[200px] relative">
+		{#if loading}
+			<LoadingOverlay text="checking and unlocking content, please wait" />
+		{/if}
+		<div class="card-header text-xl font-bold">Almost there!</div>
+		<div class="p-4 space-y-2">
+			<p>please enter the key you previously created to unlock your articles</p>
+			<form method="post" on:submit={handleSubmit}>
+				<label for="passkey">
+					<span>Passkey</span>
+					<input
+						disabled={loading}
+						bind:value={passkey}
+						type="password"
+						name="passkey"
+						autocomplete="current-password"
+						class="input"
+					/>
+				</label>
+				<button disabled={loading} type="submit" class="btn variant-filled mt-2">Submit</button>
+			</form>
+		</div>
+	</div>
+</div>
