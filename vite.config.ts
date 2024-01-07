51fba5d0788a851a7d4041ed430c18a6edeccfd7
Matteo Gassend <mgassend@gmail.com>
2024-01-07 19:25:31 +0100

    del: removed unused dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		Icons({
-			compiler: 'svelte',
-			autoInstall: true
-		})
-	]
+	plugins: [sveltekit()]
 });
