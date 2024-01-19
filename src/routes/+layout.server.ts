fd4bf8993e10361eb7837f00f7ae8670f84454d5
Matteo Gassend <mgassend@gmail.com>
2024-01-19 21:37:17 +0100

    add: conditional env for storage and openai integration

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,3 +1,4 @@
+import { env } from '$env/dynamic/private';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
@@ -7,10 +8,20 @@ export const load: LayoutServerLoad = async ({ locals }) => {
 			authed: false
 		};
 	}
+	const enabledOptions = {
+		ai: env.OPENAI_ORG && env.OPENAI_TOKEN,
+		storage:
+			env.CLOUDFLARE_ACCOUNT_ID &&
+			env.CLOUDFLARE_ACCESS_KEY_ID &&
+			env.CLOUDFLARE_BUCKET_URL &&
+			env.CLOUDFLARE_BUCKET_NAME &&
+			env.CLOUDFLARE_BUCKET_URL
+	};
 	return {
 		authed: session ? true : false,
 		userId: session.user.userId,
 		username: session.user.username,
-		email: session.user.email
+		email: session.user.email,
+		enabledOptions
 	};
 };
