e92bf7c4d866c0055661360375f471636f68a93d
Matteo Gassend <mgassend@gmail.com>
2024-01-20 14:39:59 +0100

    add: env check for api routes

--- src/routes/api/giphy/+server.ts
+++ src/routes/api/giphy/+server.ts
@@ -1,10 +1,13 @@
-import { GIPHY_TOKEN } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import { GiphyFetch } from '@giphy/js-fetch-api';
-import { json, type RequestHandler } from '@sveltejs/kit';
-
-const client = new GiphyFetch(GIPHY_TOKEN);
+import { fail, json, type RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async ({ url }) => {
+	const { GIPHY_TOKEN } = env;
+	if (!GIPHY_TOKEN) {
+		throw fail(500, { message: 'could not find giphy configuration' });
+	}
+	const client = new GiphyFetch(GIPHY_TOKEN);
 	const query = url.searchParams.get('query');
 	const pageParam = url.searchParams.get('page');
 	const page = pageParam ? parseInt(pageParam) : 0;
--- src/routes/api/unsplash/+server.ts
+++ src/routes/api/unsplash/+server.ts
@@ -1,9 +1,13 @@
-import { UNSPLASH_TOKEN } from '$env/static/private';
-import { json, type RequestHandler } from '@sveltejs/kit';
+import { env } from '$env/dynamic/private';
+import { fail, json, type RequestHandler } from '@sveltejs/kit';
 import { createApi } from 'unsplash-js';
 const client = createApi({ accessKey: UNSPLASH_TOKEN });
 
 export const GET: RequestHandler = async ({ url }) => {
+	const { UNSPLASH_TOKEN } = env;
+	if (!UNSPLASH_TOKEN) {
+		throw fail(500, { message: 'could not find unsplash configuration' });
+	}
 	const query = url.searchParams.get('query');
 	const page = url.searchParams.has('page') ? parseInt(url.searchParams.get('page')) : 1;
 	if (query) {
