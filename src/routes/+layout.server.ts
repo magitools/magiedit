c972bad7bf14a3d2dd372e2c101e3ea85fd337aa
Matteo Gassend <mgassend@gmail.com>
2024-01-20 14:38:30 +0100

    fix: parsing values of env to boolean

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -9,20 +9,26 @@ export const load: LayoutServerLoad = async ({ locals }) => {
 		};
 	}
 	const enabledOptions = {
-		ai: env.OPENAI_ORG && env.OPENAI_TOKEN,
+		ai: typeof (env.OPENAI_ORG && env.OPENAI_TOKEN) === 'string',
 		stripe:
-			env.STRIPE_KEY &&
-			env.STRIPE_CANCEL_URL &&
-			env.STRIPE_CHECKOUT_HOOK_SIGNATURE &&
-			env.STRIPE_PRICE_ID &&
-			env.STRIPE_PRODUCT_ID &&
-			env.STRIPE_SUCCESS_URL,
+			typeof (
+				env.STRIPE_KEY &&
+				env.STRIPE_CANCEL_URL &&
+				env.STRIPE_CHECKOUT_HOOK_SIGNATURE &&
+				env.STRIPE_PRICE_ID &&
+				env.STRIPE_PRODUCT_ID &&
+				env.STRIPE_SUCCESS_URL
+			) === 'string',
 		storage:
-			env.CLOUDFLARE_ACCOUNT_ID &&
-			env.CLOUDFLARE_ACCESS_KEY_ID &&
-			env.CLOUDFLARE_BUCKET_URL &&
-			env.CLOUDFLARE_BUCKET_NAME &&
-			env.CLOUDFLARE_BUCKET_URL
+			typeof (
+				env.CLOUDFLARE_ACCOUNT_ID &&
+				env.CLOUDFLARE_ACCESS_KEY_ID &&
+				env.CLOUDFLARE_BUCKET_URL &&
+				env.CLOUDFLARE_BUCKET_NAME &&
+				env.CLOUDFLARE_BUCKET_URL
+			) === 'string',
+		unsplash: typeof env.UNSPLASH_TOKEN === 'string',
+		giphy: typeof env.GIPHY_TOKEN === 'string'
 	};
 	return {
 		authed: session ? true : false,
