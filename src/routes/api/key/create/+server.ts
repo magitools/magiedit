7de73b77c9bb4238d1dc8759769acbb5da977bdb
Matteo Gassend <mgassend@gmail.com>
2024-01-27 21:25:43 +0100

    add: creating and unlocking a key sets the key cookie

--- src/routes/api/key/create/+server.ts
+++ src/routes/api/key/create/+server.ts
@@ -3,11 +3,13 @@ import { user } from '$lib/server/drizzle';
 import { fail, json, redirect, type RequestHandler } from '@sveltejs/kit';
 import bcrypt from 'bcryptjs';
 
-export const POST: RequestHandler = async ({ locals, request }) => {
+export const POST: RequestHandler = async ({ locals, request, cookies }) => {
 	const session = await locals.auth.validate();
 	if (!session) throw redirect(302, '/login');
 	const { passkey } = Object.fromEntries(await request.formData());
 	if (!passkey) throw fail(500, { message: 'invalid data' });
 	await db.update(user).set({ keyHash: await bcrypt.hash(passkey.toString(), 12) });
+	//TODO set transform string
+	cookies.set('magiedit:key', passkey.toString(), { path: '/' });
 	return json({ message: 'ok' });
 };
--- src/routes/api/key/unlock/+server.ts
+++ src/routes/api/key/unlock/+server.ts
@@ -1,12 +1,14 @@
 import { fail, json, redirect, type RequestHandler } from '@sveltejs/kit';
 import bcrypt from 'bcryptjs';
 
-export const POST: RequestHandler = async ({ locals, request }) => {
+export const POST: RequestHandler = async ({ locals, request, cookies }) => {
 	const session = await locals.auth.validate();
 	if (!session) throw redirect(302, '/login');
 	const { passkey } = Object.fromEntries(await request.formData());
 	if (!passkey) throw fail(500, { message: 'invalid data' });
 	if (!(await bcrypt.compare(passkey.toString(), session.user.keyHash)))
 		throw fail(500, { message: 'invalid credentials' });
+	//TODO set transform string
+	cookies.set('magiedit:key', passkey.toString(), { path: '/' });
 	return json({ message: 'ok' });
 };
