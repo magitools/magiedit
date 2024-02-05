d20062d66550e0a790b8ea1b61f64f81bd6b254d
Matteo Gassend <mgassend@gmail.com>
2024-02-05 08:45:03 +0100

    fix: env variables for build pipeline

--- ci/build.mjs
+++ ci/build.mjs
@@ -14,26 +14,11 @@ connect(
 		const runner = node.withWorkdir('/app').withExec(['npm', 'install', '--ignore-scripts']);
 		await runner
 			.withEnvVariable('BUILD_ON_NODE', 'y')
-			.withEnvVariable('GIPHY_TOKEN', 'giphy_token')
-			.withEnvVariable('UNSPLASH_TOKEN', 'unsplash_token')
 			.withEnvVariable('DATABASE_URL', 'libsql://fake-url.com')
 			.withEnvVariable('DATABASE_TOKEN', 'token')
 			.withEnvVariable('GITHUB_CLIENT_ID', 'id')
 			.withEnvVariable('GITHUB_CLIENT_SECRET', 'secret')
-			.withEnvVariable('OPENAI_TOKEN', 'secret')
-			.withEnvVariable('OPENAI_ORG', 'secret')
-			.withEnvVariable('CLOUDFLARE_SECRET_ACCESS_KEY', 'secret')
-			.withEnvVariable('CLOUDFLARE_ACCESS_KEY_ID', 'secret')
-			.withEnvVariable('CLOUDFLARE_ACCOUNT_ID', 'secret')
-			.withEnvVariable('CLOUDFLARE_TOKEN', 'secret')
-			.withEnvVariable('CLOUDFLARE_BUCKET_NAME', 'secret')
-			.withEnvVariable('CLOUDFLARE_BUCKET_URL', 'secret')
-			.withEnvVariable('STRIPE_KEY', 'secret')
-			.withEnvVariable('STRIPE_PRODUCT_ID', 'secret')
-			.withEnvVariable('STRIPE_PRICE_ID', 'secret')
-			.withEnvVariable('STRIPE_SUCCESS_URL', 'secret')
-			.withEnvVariable('STRIPE_CANCEL_URL', 'secret')
-			.withEnvVariable('STRIPE_CHECKOUT_HOOK_SIGNATURE', 'secret')
+			.withEnvVariable('COOKIE_ENCRYPTION_KEY', 'change_this_in_prod')
 			.withExec(['npm', 'run', 'build'])
 			.stderr();
 	},
