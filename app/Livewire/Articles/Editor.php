e572b585354fe83d8ab5fa3ee1c5f8ad1b7636b8
Matteo Gassend <mgassend@gmail.com>
2024-10-15 09:01:26 +0200

    add: create article wip

--- app/Livewire/Articles/Editor.php
+++ app/Livewire/Articles/Editor.php
@@ -2,13 +2,16 @@
 
 namespace App\Livewire\Articles;
 
+use App\Models\Article;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Editor extends Component
 {
     public string $content = "write your article here";
-    public bool $initialLoad = true;
+    protected ?Article $article = null;
 
     #[Computed]
     public function frontmatter() {
@@ -16,7 +19,14 @@ class Editor extends Component
     }
 
     public function save(string $content) {
-        dd($content);
+        $parsed = YamlFrontMatter::parse($content);
+        $title = $parsed->matter('title');
+        if ($this->article) {
+            $this->article->update(['content' => $content, 'fm' => $parsed->matter, 'title' => $title]);
+        } else {
+            $title = $title ? $title : fake()->unique()->realText(16);
+            $this->article = Auth::user()->articles()->create(['content' => $content, 'fm' => json_encode($parsed->matter), 'title' => $title]);
+        }
     }
 
     public function render()
