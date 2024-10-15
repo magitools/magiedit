52e626a5f2ee70048873bc8191167bb008631110
Matteo Gassend <mgassend@gmail.com>
2024-10-15 11:04:52 +0200

    fix: actually storing frontmatter data

--- app/Livewire/Articles/Editor.php
+++ app/Livewire/Articles/Editor.php
@@ -10,22 +10,25 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Editor extends Component
 {
-    public string $content = "write your article here";
+    public string $content = 'write your article here';
+
     protected ?Article $article = null;
 
     #[Computed]
-    public function frontmatter() {
+    public function frontmatter()
+    {
         return [];
     }
 
-    public function save(string $content) {
+    public function save(string $content)
+    {
         $parsed = YamlFrontMatter::parse($content);
         $title = $parsed->matter('title');
         if ($this->article) {
-            $this->article->update(['content' => $content, 'fm' => $parsed->matter, 'title' => $title]);
+            $this->article->update(['content' => $content, 'fm' => $parsed->matter(), 'title' => $title]);
         } else {
             $title = $title ? $title : fake()->unique()->realText(16);
-            $this->article = Auth::user()->articles()->create(['content' => $content, 'fm' => json_encode($parsed->matter), 'title' => $title]);
+            $this->article = Auth::user()->articles()->create(['content' => $content, 'fm' => $parsed->matter(), 'title' => $title]);
         }
     }
 
