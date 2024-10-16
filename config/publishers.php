1f4abe1f2c7a7ac8f446f79c8d1a562c66db9f10
Matteo Gassend <mgassend@gmail.com>
2024-10-16 12:02:35 +0200

    add: required field validation wip

--- config/publishers.php
+++ config/publishers.php
@@ -5,4 +5,23 @@ return [
         'input',
         'select',
     ],
+    'field_validation' => [
+        'input' => [
+            'required_values' => [
+                'type',
+                'label'
+            ],
+            'optional_values' => [
+                'placeholder'
+            ]
+        ],
+        'select' => [
+            'required_values' => [
+
+            ],
+            'optional_values' => [
+
+            ]
+        ]
+    ]
 ];
--- tests/Feature/PublisherTest.php
+++ tests/Feature/PublisherTest.php
@@ -8,12 +8,29 @@ describe('publishers', function () {
         $this->classes = Discover::in(app_path('Publishers'))->classes()->implementing(PublisherContract::class)->get();
     });
 
-    it('publisher inputs are all valid', function () {
+    arch()
+        ->expect('App\Publishers')
+        ->toBeClasses()
+        ->ignoring('App\Publishers\PublisherContract');
+
+    it('publisher inputs are allowed', function () {
         foreach ($this->classes as $class) {
             $instance = new $class;
             $inputs = $instance->getInputs();
             foreach ($inputs as $input) {
+                $validationRules = config('publishers.field_validation');
                 expect($input['input'])->toBeIn(config('publishers.allowed_fields'));
+                expect($input)->toHaveKeys($validationRules[$input['input']]['required_values']);
+            }
+        }
+    });
+    it('publisher inputs have all required options', function() {
+        foreach ($this->classes as $class) {
+            $instance = new $class;
+            $inputs = $instance->getInputs();
+            foreach ($inputs as $input) {
+                $validationRules = config('publishers.field_validation');
+                expect($input)->toHaveKeys($validationRules[$input['input']]['required_values']);
             }
         }
     });
