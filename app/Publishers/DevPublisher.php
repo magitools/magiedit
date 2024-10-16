e7ac2cadb9704f8bfe32ec60f8b159a97c9b7f71
Matteo Gassend <mgassend@gmail.com>
2024-10-16 09:07:22 +0200

    add: DevPublisher wip

--- app/Publishers/DevPublisher.php
+++ app/Publishers/DevPublisher.php
@@ -6,6 +6,19 @@ class DevPublisher implements PublisherContract
 {
     public function getName(): string
     {
-        return "dev.to";
+        return 'dev.to';
+    }
+
+    public function getInputs(): array
+    {
+        return [
+            [
+                'name' => 'api-key',
+                'input' => 'input',
+                'type' => 'text',
+                'label' => 'Enter your dev.to api key',
+                'placeholder' => 'type your api key here...',
+            ],
+        ];
     }
 }
