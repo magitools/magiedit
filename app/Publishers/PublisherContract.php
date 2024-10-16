fac6586c649c639fdca886d25029afa407fc8431
Matteo Gassend <mgassend@gmail.com>
2024-10-16 18:41:44 +0200

    add: more methods on publisher interface

--- app/Publishers/PublisherContract.php
+++ app/Publishers/PublisherContract.php
@@ -5,6 +5,18 @@ namespace App\Publishers;
 interface PublisherContract
 {
     public function getName(): string;
-
+    /**
+     * @return void
+     */
     public function getInputs(): array;
+    /**
+     * @param array<int,mixed> $values
+     */
+    public function setData(array $values): self;
+    /**
+     * @param array<int,mixed> $fm
+     */
+    public function setFm(array $fm): self;
+
+    public function publish(string $content): bool;
 }
