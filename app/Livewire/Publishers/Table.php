c6932e87f0e8a4c2e852c2ce9e875ab5ef02d38b
Matteo Gassend <mgassend@gmail.com>
2024-10-16 15:28:04 +0200

    add: delete works & navigation to get to create publisher

--- app/Livewire/Publishers/Table.php
+++ app/Livewire/Publishers/Table.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Publishers;
 
+use App\Models\Publisher;
 use Flux\Flux;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
@@ -19,14 +20,21 @@ class Table extends Component
         Flux::modal('delete-publisher')->show();
     }
 
+    public function getPublishers()
+    {
+        $this->publishers = Auth::user()->publishers;
+    }
+
     public function destroyPublisher()
     {
         Flux::modal('delete-publisher')->close();
+        Publisher::destroy([$this->selectedPublisher]);
+        $this->getPublishers();
         Flux::toast('publisher deleted');
     }
 
     public function mount() {
-        $this->publishers = Auth::user()->publishers;
+        $this->getPublishers();
     }
 
     public function render()
--- resources/views/publishers/index.blade.php
+++ resources/views/publishers/index.blade.php
@@ -1,3 +1,8 @@
 <x-layouts.app>
+    <div class="flex">
+        <flux:spacer />
+        <flux:button variant="primary" href="{{route('app.publishers.create')}}">Create Püblisher</flux:button>
+    </div>
+
     <livewire:publishers.table />
 </x-layouts.app>
