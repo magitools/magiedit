c1ccf2c6f9e7da6c16cdf8ca78d74ff569d5ce9c
Matteo Gassend <mgassend@gmail.com>
2024-10-21 13:56:23 +0200

    add: api routes wip

--- app/Http/Controllers/PublisherController.php
+++ app/Http/Controllers/PublisherController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PublisherResource;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 
 class PublisherController extends Controller
 {
@@ -14,5 +17,11 @@ class PublisherController extends Controller
     public function create(): View
     {
         return view('publishers.create');
+    }
+
+    public function indexApi() {
+            return response()->json([
+                'publishers' => PublisherResource::collection(Auth::user()->publishers)
+            ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\PublisherController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware(['auth:sanctum'])->group(function () {
+   Route::prefix('publishers')->group(function () {
+      Route::get('', [PublisherController::class, 'indexApi']);
+   });
+});
