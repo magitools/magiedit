f24dd186238586aecc13253630ed799806656cf0
Matteo Gassend <mgassend@gmail.com>
2024-10-22 15:44:52 +0200

    add: publish API route

--- app/Http/Controllers/PublisherController.php
+++ app/Http/Controllers/PublisherController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\PublisherResource;
+use App\Models\Publisher;
+use App\Publishers\PublisherContract;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
 
 class PublisherController extends Controller
 {
@@ -19,9 +25,35 @@ class PublisherController extends Controller
         return view('publishers.create');
     }
 
-    public function indexApi() {
+    public function indexApi(): JsonResponse
+    {
             return response()->json([
                 'publishers' => PublisherResource::collection(Auth::user()->publishers)
             ]);
+    }
+
+    public function publishApi(Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+           'publishers' => 'required|array',
+            'content' => 'required'
+        ]);
+        $parsed = YamlFrontMatter::parse($validated['content']);
+        Log::info($validated['content']);
+        Log::info(implode(',', $validated['publishers']));
+        $publishers = Publisher::query()->whereIn('id', $validated['publishers'])->get();
+        Log::info(count($publishers));
+        $res = [];
+        foreach ($publishers as $publisher) {
+            $className = $publisher->class_name;
+            /** @var PublisherContract */
+            $instance = new $className();
+            $valid = $instance
+                ->setData($publisher->data)
+                ->setFm($parsed->matter())
+                ->publish($validated['content']);
+            $res[$publisher->name] = $valid;
+        }
+        return response()->json(['status' => $res]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->group(function () {
    Route::prefix('publishers')->group(function () {
       Route::get('', [PublisherController::class, 'indexApi']);
+      Route::post('/publish', [PublisherController::class, 'publishApi']);
    });
 });
