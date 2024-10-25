bff79a17736668d806fe255832906d06c2d44352
Matteo Gassend <mgassend@gmail.com>
2024-10-25 15:16:48 +0200

    add: landing page wip

--- config/newsletter.php
+++ config/newsletter.php
@@ -5,5 +5,5 @@ return [
     'username' => env('LISTMONK_USERNAME'),
     'password' => env('LISTMONK_PASSWORD'),
     'url' => env('LISTMONK_URL'),
-    'listId' => env('LISTMONK_LISTID')
+    'listId' => env('LISTMONK_LISTID', [3, 4])
 ];
