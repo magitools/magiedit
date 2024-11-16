d652a19428ffb2a5090b5d11449f4ccf3b4016f8
Matteo Gassend <mgassend@gmail.com>
2024-11-16 18:55:21 +0100

    add: solo

---
+++ app/Providers/SoloServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use AaronFrancis\Solo\Commands\EnhancedTailCommand;
+use AaronFrancis\Solo\Facades\Solo;
+use AaronFrancis\Solo\Providers\SoloApplicationServiceProvider;
+
+class SoloServiceProvider extends SoloApplicationServiceProvider
+{
+    public function register()
+    {
+        Solo::useTheme('dark')
+            // Commands that auto start.
+            ->addCommands([
+                'Logs' => 'php artisan pail',
+                'Vite' => 'npm run dev',
+                 'HTTP' => 'php artisan serve',
+            ])
+            // Not auto-started
+            ->addLazyCommands([
+                'Queue' => 'php artisan queue:listen --tries=1',
+                // 'Reverb' => 'php artisan reverb:start',
+                // 'Pint' => 'pint --ansi',
+            ])
+            // FQCNs of trusted classes that can add commands.
+            ->allowCommandsAddedFrom([
+                //
+            ]);
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
