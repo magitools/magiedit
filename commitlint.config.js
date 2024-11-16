ce0b95ab7132dad4a5f0de175e927cd53ac7a510
Matteo Gassend <mgassend@gmail.com>
2024-11-16 19:19:59 +0100

    add: custom rules for commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,19 @@
+import { RuleConfigSeverity } from "@commitlint/types"
+
 export default {
     extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            RuleConfigSeverity.Error,
+            'always',
+            [
+                'add',
+                'feat',
+                'del',
+                'fix',
+                'chore',
+                'docs'
+            ]
+        ]
+    }
 };
