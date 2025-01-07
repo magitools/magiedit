afb9ecd3950f64d6aea5ae6c79e6bac24f9efe4e
Matteo Gassend <matteo@Matteos-MacBook-Pro.local>
2025-01-07 16:37:18 +0100

    del: removed laravel application

--- config/newsletter.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-return [
-    'username' => env('LISTMONK_USERNAME'),
-    'password' => env('LISTMONK_PASSWORD'),
-    'url' => env('LISTMONK_URL'),
-    'listId' => env('LISTMONK_LISTID', [3, 4])
-];
